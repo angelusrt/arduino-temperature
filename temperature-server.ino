7051ef506394735833e42af59112c14b261c5c4d
Angelus Rocha <angelusrt@gmail.com>
2024-04-12 23:10:53 -0300

    adicionando otimizações e estatística

--- temperature-server.ino
+++ temperature-server.ino
@@ -7,16 +7,17 @@
 #define tx_pin 6
 #define dht_data_pin 13
 #define dht_energy_pin 12
+// #define time_to_update 30000 
 #define time_to_update 3600000 
-#define precision 0 
-#define num_size 2
 #define text_size 5
-#define data_size 10
-#define cat_size 31
+#define data_size 5
+#define cat_size 21
+#define int_base 10
+#define cmd_size 128
 
 dht DHT;
 SoftwareSerial wifi_serial(rx_pin, tx_pin);
-String wifi_serial_cmd = "";
+char wifi_serial_cmd[cmd_size] = "";
 unsigned long start_time = 0;
 unsigned short temperatures[data_size] = {0};
 unsigned short humidities[data_size] = {0};
@@ -31,9 +32,13 @@ void setup() {
   wifi_serial.begin(115200);
 
   wifi_serial.println("AT");
+  delay(rate);
   wifi_serial.println("AT+CWMODE=1");
+  delay(rate);
   wifi_serial.println("AT+CIPMUX=1");
+  delay(rate);
   wifi_serial.println("AT+CIFSR");
+  delay(rate);
   wifi_serial.println("AT+CIPSERVER=1,80");
   Serial.println(wifi_serial.readString());
 }
@@ -52,13 +57,14 @@ void loop() {
   }
 
   if (wifi_serial.available()) {
-    wifi_serial_cmd = wifi_serial.readString();
+    wifi_serial.readStringUntil('\n').toCharArray(wifi_serial_cmd, cmd_size);
     Serial.println(wifi_serial_cmd);
   }
 
-  if (wifi_serial_cmd.indexOf('\n') > -1 && wifi_serial_cmd.indexOf("+IPD") != -1) {
+  if (strstr(wifi_serial_cmd, "+IPD") != NULL) {
     char r[512] = "";
     construct_html(r);
+    Serial.println(r);
 
     char cmd[32] = "";
     sprintf(cmd, "AT+CIPSEND=0,%d", strlen(r));
@@ -69,33 +75,58 @@ void loop() {
     delay(100);
     wifi_serial.println("AT+CIPCLOSE=0");
 
-    wifi_serial_cmd = "";
+    wifi_serial_cmd[0] = '\0';
   }
 }
 
 void construct_html(char* html) {
   html[0] = '\0';
-  char temp[text_size] = "nada", hum[text_size] = "nada", temps[cat_size] = "nada", hums[cat_size] = "nada";
+  char temp[text_size] = "nada", 
+       hum[text_size] = "nada", 
+       temps[cat_size] = "nada", 
+       temp_mean[text_size] = "nada", 
+       hum_mean[text_size] = "nada",
+       temp_range[text_size] = "nada", 
+       hum_range[text_size] = "nada",
+       buff[text_size] = "";
 
   if (registered != -1){
-    dtostrf(temperatures[index], num_size, precision, temp);
-    strncat(temp, "C ", text_size);
-    dtostrf(humidities[index], num_size, precision, hum);
-    strncat(hum, "% ", text_size);
+    itoa(temperatures[index], temp, int_base);
+    strncat(temp, "C", text_size);
+    itoa(humidities[index], hum, int_base);
+    strncat(hum, "%", text_size);
 
-    temps[0] = '\0'; hums[0] = '\0';
+    temps[0] = '\0'; 
+    short temp_total = 0, 
+          hum_total = 0,
+          temp_min = temperatures[index], 
+          hum_min = humidities[index], 
+          temp_max = 0, 
+          hum_max = 0;
 
-    for (int i = 0; i < registered+1; i++) {
-      char temp[text_size];
-      dtostrf(temperatures[index], num_size, precision, temp);
-      strncat(temp, "C ", text_size);
-      strncat(temps, temp, cat_size);
+    for (size_t i = 0; i < registered+1; i++) {
+      itoa(temperatures[i], buff, int_base);
+      strncat(buff, "C ", text_size);
+      strncat(temps, buff, cat_size);
 
-      char hum[text_size];
-      dtostrf(humidities[index], num_size, precision, hum);
-      strncat(hum, "% ", text_size);
-      strncat(hums, hum, cat_size);
+      temp_total += temperatures[i];
+      hum_total += humidities[i];
+
+      if (temperatures[i] < temp_min) { temp_min = temperatures[i]; }
+      if (temperatures[i] > temp_max) { temp_max = temperatures[i]; }
+      if (humidities[i] < hum_min) { hum_min = humidities[i]; }
+      if (humidities[i] > hum_max) { hum_max = humidities[i]; }
     }
+    
+    itoa(temp_total/(registered+1), temp_mean, int_base);
+    strncat(temp_mean, "C", text_size);
+    itoa(hum_total/(registered+1), hum_mean, int_base);
+    strncat(hum_mean, "%", text_size);
+
+    itoa(temp_max - temp_min, temp_range, int_base);
+    strncat(temp_range, "C", text_size);
+    itoa(hum_max - hum_min, hum_range, int_base);
+    strncat(hum_range, "%", text_size);
   } 
 
   sprintf(html,
@@ -103,24 +134,24 @@ void construct_html(char* html) {
     "<html lang=\"pt\" charset=\"utf-8\">"
     "<head>"
     "<style>"
-    "body { width: 80%%; font-family: sans-serif; margin: 40px auto;}"
-    "h1, h2, h3, h4 { font-weight: 500; }"
-    "h1 { margin-bottom: 0px; }"
-    "h2 { margin-top: 40px; margin-bottom: 20px; }"
+    "body{width:80%%;font-family:sans-serif;margin:40px auto;}"
+    "h1,h2,h3,h4{font-weight:500;}"
+    "h1{margin-bottom:0px;}"
+    "h2{margin-top:40px;margin-bottom:20px;}"
     "</style>"
     "</head>"
     "<body>"
-    "<h4>Os dados (maximo: 10) sao capturados a cada 1h</h4>"
+    "<h4>Os dados (max: 5) sao capturados a cada 1h</h4>"
     "<h1>Metricas atuais</h1>"
-    "<h3>"
-    "Temperatura: %s, Umidade: %s."
-    "</h3>"
-    "<h2>Temperatura registradas</h2>"
+    "<h3>%s, %s.</h3>"
+    "<h2>Dados registrados</h2>"
     "<h3>%s</h3>"
-    "<h2>Umidade registradas</h2>"
-    "<h3>%s</h3>"
+    "<h2>Medias</h2>"
+    "<h3>%s, %s.</h3>"
+    "<h2>Intervalo</h2>"
+    "<h3>%s, %s.</h3>"
     "</body>"
     "</html>",
-    temp, hum, temps, hums
+    temp, hum, temps, temp_mean, hum_mean, temp_range, hum_range
   );
 }
